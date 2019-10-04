07e90f949cf526d3f34e8bc3c7c96ec284b2e5c7
SingularityA <singularity.m101@gmail.com>
2019-10-04 19:25:32 +0300

    Extended Heap interface from BasicCollection

--- src/main/kotlin/ru/spbu/datastructures/heap/Heap.kt
+++ src/main/kotlin/ru/spbu/datastructures/heap/Heap.kt
@@ -1,16 +1,18 @@
 package ru.spbu.datastructures.heap
 
+import ru.spbu.datastructures.BasicCollection
+
 /**
- * A modifiable collection that holds pairs of objects (keys and values) and supports efficiently retrieving
- * the top value of collection. Heap keys are unique; the heap holds only one value for each key.
+ * A modifiable collection that holds pairs of objects (keys and values)
+ * and supports efficiently retrieving the top value of collection.
  * @param K the type of heap keys.
  * @param V the type of heap values.
  */
-interface Heap<K, V>: MutableMap<K, V> {
+interface Heap<K, V>: BasicCollection<K> {
 
     /**
      * Returns the value of the key on top of the heap,
-     * does not remove the key and its corresponding value from the heap
+     * does not remove the key and its corresponding value from the heap.
      *
      * @return the value associated with the top key, or `null` if the heap is empty.
      */
@@ -18,30 +20,43 @@ interface Heap<K, V>: MutableMap<K, V> {
 
     /**
      * Extracts the value of the key on top of the heap,
-     * removing the key and its corresponding value from the heap
+     * removing the key and its corresponding value from the heap.
      *
      * @return the value associated with the top key, or `null` if the heap is empty.
      */
     fun pop(): V?
 
     /**
-     * Pops the top element and puts the new pair of [key] and [value]
-     * More efficient than pop followed by put, since only need to balance once
+     * Adds new [key] and [value] associated with it to the heap.
+     */
+    fun push(key: K, value: V)
+
+    /**
+     * Pops the top element and puts new pair of [key] and [value].
+     * More efficient than pop followed by put, since only need to balance once.
+     * If the heap is empty the pair would still be added to the heap.
      *
-     * @return the value associated with the top key, or `null` if the heap is empty.
+     * @return the value associated with the old top key, or `null` if the heap is empty.
      */
     fun replace(key: K, value: V): V?
 
     /**
+     * Removes the pair with the [key].
+
+     * @return the value associated with the [key], or `null` if there is no such key or the heap is empty.
+     */
+    fun remove(key: K): V?
+
+    /**
      * Joins two heaps to form a valid new heap containing all the elements of both, preserving the original heaps.
      *
-     * @return the new heap containing elements of this heap and [heap].
+     * @return new heap containing elements of this heap and [heap].
      */
     fun merge(heap: Heap<K, V>): Heap<K, V>
 
     /**
      * Joins two heaps to form a valid new heap containing all the elements of both, destroying the original heaps.
-     * (Mutates this and [heap])
+     * (Mutates this and [heap]).
      *
      * @return this
      */
@@ -49,7 +64,7 @@ interface Heap<K, V>: MutableMap<K, V> {
 
     /**
      * Changes the key of the element from [oldKey] to [newKey].
-     * The [newKey] is assumed to be higher than the [oldKey]
+     * The [newKey] is assumed to be higher than the [oldKey].
      *
      * @return the assigned [newKey], or `null` if the [oldKey] was not present in the heap.
      */
