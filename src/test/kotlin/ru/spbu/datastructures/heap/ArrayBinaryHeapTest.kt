0778d028a46db7e7393d0f3ec68ca96238861a0d
SingularityA <singularity.m101@gmail.com>
2019-11-20 18:32:29 +0300

    Renewed tests and added some new tests for array binary heap

--- src/test/kotlin/ru/spbu/datastructures/heap/ArrayBinaryHeapTest.kt
+++ src/test/kotlin/ru/spbu/datastructures/heap/ArrayBinaryHeapTest.kt
@@ -1,6 +1,6 @@
 package ru.spbu.datastructures.heap
 
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 import java.util.Comparator
 
@@ -11,91 +11,284 @@ class ArrayBinaryHeapTest {
 
     @Test
     fun testPeek() {
-        val emptyHeap = ArrayBinaryHeap<Int, Int>()
-        Assert.assertEquals(null, emptyHeap.peek())
-
-        val heap = ArrayBinaryHeap(listOf(3, 2, 1, 4, 5), listOf(33, 22, 11, 44, 55))
-        Assert.assertEquals(11, heap.peek())
+        val heap = IntArrayBinaryHeap(
+            listOf(3, 2, 1, 4, 5),
+            listOf(33, 22, 11, 44, 55)
+        )
+        assertEquals(11, heap.peek())
     }
 
     @Test
-    fun testComparator() {
-        val heap = ArrayBinaryHeap(listOf(3, 2, 1, 4, 5), listOf(33, 22, 11, 44, 55), Comparator.reverseOrder())
-        Assert.assertEquals(55, heap.peek())
+    fun testPeekComparator() {
+        val heap = IntArrayBinaryHeap(
+            listOf(3, 2, 1, 4, 5),
+            listOf(33, 22, 11, 44, 55),
+            Comparator.reverseOrder()
+        )
+        assertEquals(55, heap.peek())
+    }
+
+    @Test
+    fun testPeekEmpty() {
+        val emptyHeap = IntArrayBinaryHeap<Int>()
+        assertEquals(null, emptyHeap.peek())
+        assertEquals(0, emptyHeap.size)
     }
 
     @Test
     fun testPush() {
-        val heap: Heap<Int, Int> = ArrayBinaryHeap()
-
+        val heap = IntArrayBinaryHeap<Int>()
         (10 downTo 1).onEach { heap.push(it, it * 11) }
 
-        Assert.assertEquals(10, heap.size)
-        Assert.assertEquals(11, heap.peek())
+        assertEquals(10, heap.size)
+        assertEquals(11, heap.peek())
+    }
+
+    @Test
+    fun testPushComparator() {
+        val heap = IntArrayBinaryHeap<Int>(reverseOrder())
+        (10 downTo 1).onEach { heap.push(it, it * 11) }
+
+        assertEquals(10, heap.size)
+        assertEquals(110, heap.peek())
     }
 
     @Test
     fun testPop() {
-        val heap = ArrayBinaryHeap(listOf(3, 2, 1, 4, 5), listOf(33, 22, 11, 44, 55))
+        val heap = IntArrayBinaryHeap(
+            listOf(3, 2, 1, 4, 5),
+            listOf(33, 22, 11, 44, 55)
+        )
 
         val popped = heap.pop()
 
-        Assert.assertEquals(11, popped)
-        Assert.assertEquals(4, heap.size)
-        Assert.assertEquals(22, heap.peek())
+        assertEquals(11, popped)
+        assertEquals(4, heap.size)
+        assertEquals(22, heap.peek())
+    }
+
+    @Test
+    fun testPopComparator() {
+        val heap = IntArrayBinaryHeap(
+            listOf(3, 2, 1, 4, 5),
+            listOf(33, 22, 11, 44, 55),
+            reverseOrder()
+        )
+
+        val popped = heap.pop()
+
+        assertEquals(55, popped)
+        assertEquals(4, heap.size)
+        assertEquals(44, heap.peek())
+    }
+
+    @Test
+    fun testPopEmpty() {
+        val emptyHeap = IntArrayBinaryHeap<Int>()
+
+        val popped = emptyHeap.pop()
+
+        assertEquals(null, popped)
+        assertEquals(0, emptyHeap.size)
     }
 
     @Test
     fun testReplace() {
-        val heap = ArrayBinaryHeap(listOf(1, 2, 3), listOf(11, 22, 33))
-        val key = 4
-        val value = 44
+        val heap = IntArrayBinaryHeap(
+            listOf(1, 2, 3, 4, 5),
+            listOf(11, 22, 33, 44, 55)
+        )
 
-        val popped = heap.replace(4, 44)
+        val replaced = heap.replace(6, 66)
 
         println(heap)
-        Assert.assertEquals(3, heap.size)
-        Assert.assertEquals(22, heap.peek())
+        assertEquals(5, heap.size)
+        assertEquals(22, heap.peek())
+        assertEquals(11, replaced)
+    }
+
+    @Test
+    fun testReplaceComparator() {
+        val heap = IntArrayBinaryHeap(
+            listOf(6, 5, 4, 3, 2),
+            listOf(66, 55, 44, 33, 22),
+            reverseOrder()
+        )
+
+        val replaced = heap.replace(1, 11)
+
+        println(heap)
+        assertEquals(5, heap.size)
+        assertEquals(55, heap.peek())
+        assertEquals(66, replaced)
+    }
+
+    @Test
+    fun testReplaceEmpty() {
+        val emptyHeap = IntArrayBinaryHeap<Int>()
+
+        val replaced = emptyHeap.replace(1, 11)
+        println(emptyHeap)
+
+        assertEquals(null, replaced)
+        assertEquals(1, emptyHeap.size)
+        assertEquals(11, emptyHeap.peek())
     }
 
     @Test
     fun testRemove() {
-        val heap = ArrayBinaryHeap(listOf(1, 2, 3, 4, 5, 6, 7), listOf(11, 22, 33, 44, 55, 66, 77))
+        val heap = IntArrayBinaryHeap(
+            listOf(1, 2, 3, 4, 5, 6, 7),
+            listOf(11, 22, 33, 44, 55, 66, 77)
+        )
         println(heap)
 
-        val removed = heap.remove(2)
+        var removed = heap.remove(22)
         println(heap)
 
-        Assert.assertEquals(6, heap.size)
-        Assert.assertEquals(11, heap.peek())
-        Assert.assertEquals(22, removed)
+        assertEquals(6, heap.size)
+        assertEquals(11, heap.peek())
+        assertEquals(22, removed)
+
+        removed = heap.remove(55)
+        println(heap)
+
+        assertEquals(5, heap.size)
+        assertEquals(11, heap.peek())
+        assertEquals(55, removed)
+    }
+
+    @Test
+    fun testRemoveComparator() {
+        val heap = IntArrayBinaryHeap(
+            listOf(5, 4, 3, 2, 1),
+            listOf(55, 44, 33, 22, 11),
+            reverseOrder()
+        )
+        println(heap)
+
+        var removed = heap.remove(22)
+        println(heap)
+
+        assertEquals(4, heap.size)
+        assertEquals(55, heap.peek())
+        assertEquals(22, removed)
+
+        removed = heap.remove(55)
+        println(heap)
+
+        assertEquals(3, heap.size)
+        assertEquals(44, heap.peek())
+        assertEquals(55, removed)
+    }
+
+    @Test
+    fun testRemoveEmpty() {
+        val emptyHeap = IntArrayBinaryHeap<Int>()
+        val removed = emptyHeap.remove(11)
+
+        assertEquals(null, removed)
+    }
+
+    @Test
+    fun testPrioritize() {
+        val heap = IntArrayBinaryHeap(
+            listOf(2, 3, 4, 5, 6, 7),
+            listOf(22, 33, 44, 55, 66, 77)
+        )
+
+        val prioritized = heap.prioritize(7, 1)
+
+        println(heap)
+        assertEquals(1, prioritized)
+        assertEquals(77, heap.peek())
+        assertEquals(6, heap.size)
+    }
+
+    @Test
+    fun testPrioritizeComparator() {
+        val heap = IntArrayBinaryHeap(
+            listOf(5, 4, 3, 2, 1),
+            listOf(55, 44, 33, 22, 11),
+            reverseOrder()
+        )
+
+        val prioritized = heap.prioritize(1, 6)
+
+        println(heap)
+        assertEquals(6, prioritized)
+        assertEquals(11, heap.peek())
+    }
+
+    @Test
+    fun testPrioritizeEmpty() {
+        val emptyHeap = IntArrayBinaryHeap<Int>()
+
+        val prioritized = emptyHeap.prioritize(2, 1)
+
+        println(emptyHeap)
+        assertEquals(null, prioritized)
+        assertEquals(0, emptyHeap.size)
+        assertEquals(null, emptyHeap.peek())
     }
 
     @Test
     fun testMerge() {
-        val heap1 = ArrayBinaryHeap(listOf(1, 2, 3), listOf(11, 22, 33))
-        val heap2 = ArrayBinaryHeap(listOf(4, 5, 6, 7, 8), listOf(44, 55, 66, 77, 88))
+        val heap = IntArrayBinaryHeap(listOf(1, 2, 3), listOf(11, 22, 33))
+        val anotherHeap = IntArrayBinaryHeap(listOf(4, 5, 6, 7, 8), listOf(44, 55, 66, 77, 88))
 
-        val mergedHeap = heap1.merge(heap2)
+        val mergedHeap = heap.merge(anotherHeap)
 
-        println(heap1)
-        println(heap2)
+        println(heap)
+        println(anotherHeap)
         println(mergedHeap)
 
-        Assert.assertNotSame(heap1, mergedHeap)
+        assertNotSame(heap, anotherHeap)
+        assertEquals(8, mergedHeap.size)
+    }
+
+    @Test
+    fun testMergeEmpty() {
+        val heap = IntArrayBinaryHeap<Int>()
+        val anotherHeap = IntArrayBinaryHeap<Int>()
+
+        val mergedHeap = heap.merge(anotherHeap)
+
+        println(heap)
+        println(anotherHeap)
+        println(mergedHeap)
+
+        assertNotSame(heap, anotherHeap)
+        assertTrue(mergedHeap.isEmpty())
     }
 
     @Test
     fun testMeld() {
-        val heap1 = ArrayBinaryHeap(listOf(1, 2, 3), listOf(11, 22, 33))
-        val heap2 = ArrayBinaryHeap(listOf(4, 5, 6, 7, 8), listOf(44, 55, 66, 77, 88))
+        val heap = IntArrayBinaryHeap(listOf(1, 2, 3), listOf(11, 22, 33))
+        val anotherHeap = IntArrayBinaryHeap(listOf(4, 5, 6, 7, 8), listOf(44, 55, 66, 77, 88))
 
-        val meldedHeap = heap1.meld(heap2)
+        val meldedHeap = heap.meld(anotherHeap)
 
-        println(heap1)
-        println(heap2)
+        println(heap)
+        println(anotherHeap)
         println(meldedHeap)
 
-        Assert.assertSame(heap1, meldedHeap)
+        assertSame(heap, meldedHeap)
+        assertEquals(8, meldedHeap.size)
+    }
+
+    @Test
+    fun testMeldEmpty() {
+        val heap = IntArrayBinaryHeap<Int>()
+        val anotherHeap = IntArrayBinaryHeap<Int>()
+
+        val meldedHeap = heap.merge(anotherHeap)
+
+        println(heap)
+        println(anotherHeap)
+        println(meldedHeap)
+
+        assertNotSame(heap, anotherHeap)
+        assertTrue(meldedHeap.isEmpty())
     }
 }
