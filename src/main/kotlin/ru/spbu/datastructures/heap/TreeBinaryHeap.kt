c248adcc711d3269552fae0277b0f140993107d0
SingularityA <singularity.m101@gmail.com>
2019-11-22 05:23:51 +0300

    Renewed tree binary heap implementation and tests

--- src/main/kotlin/ru/spbu/datastructures/heap/TreeBinaryHeap.kt
+++ src/main/kotlin/ru/spbu/datastructures/heap/TreeBinaryHeap.kt
@@ -1,6 +1,7 @@
 package ru.spbu.datastructures.heap
 
 import kotlin.math.log2
+import kotlin.math.pow
 
 /**
  * @author SingularityA
@@ -12,16 +13,41 @@ class TreeBinaryHeap<K : Comparable<K>, V>(
     private var head: Node<K, V>? = null
 
     override val keys: List<K>
-        get() = TODO("not implemented")
+        get() = traverse().map { node -> node.key }
 
     override val values: List<V>
-        get() = TODO("not implemented")
+        get() = traverse().map { node -> node.value }
 
     override var size: Int = 0
         private set
 
     private val last: Node<K, V>?
         get() = if (isEmpty()) null else getElement(size - 1)
+
+    constructor(
+        keys: Collection<K>,
+        values: Collection<V>,
+        comparator: Comparator<K> = naturalOrder()
+    ) : this(comparator) {
+
+        require(keys.size == values.size) { "Keys and values collections must have equal size" }
+        if (keys.isNotEmpty()) {
+            (keys zip values).map { pair -> pushRaw(pair.first, pair.second) }
+            heapify()
+        }
+    }
+
+    private fun pushRaw(key: K, value: V) {
+        val node = Node(key, value)
+        if (isEmpty()) head = node
+        else {
+            val parent = getElement((size - 1) / 2)
+            node.parent = parent
+            if (parent.left == null) parent.left = node
+            else parent.right = node
+        }
+        size += 1
+    }
 
     override fun peek(): V? = head?.value
 
@@ -34,10 +60,7 @@ class TreeBinaryHeap<K : Comparable<K>, V>(
         } else {
             val tail = last!!
             swapContent(head!!, tail)
-            if (size % 2 == 1)
-                tail.parent!!.right = null
-            else
-                tail.parent!!.left = null
+            abandon(tail)
             siftDown(head!!)
         }
         size--
@@ -62,9 +85,9 @@ class TreeBinaryHeap<K : Comparable<K>, V>(
             head = Node(key, value)
             null
         } else {
-            val oldValue = head?.value
-            head?.key = key
-            head?.value = value
+            val oldValue = head!!.value
+            head!!.key = key
+            head!!.value = value
 
             siftDown(head!!)
             oldValue
@@ -72,11 +95,28 @@ class TreeBinaryHeap<K : Comparable<K>, V>(
     }
 
     override fun remove(value: V): V? {
-        TODO("not implemented")
+        val node = find { node -> node.value == value } ?: return null
+
+        if (size == 1) {
+            head = null
+        } else {
+            val tail = last!!
+            swapContent(node, tail)
+            abandon(tail)
+            siftDown(node)
+        }
+        size--
+        return value
     }
 
     override fun prioritize(oldKey: K, newKey: K): K? {
-        TODO("not implemented")
+        require(comparator.compare(oldKey, newKey) >= 0) { "New key must be higher than old key" }
+        val node = find { node -> node.key == oldKey } ?: return null
+
+        node.key = newKey
+        siftUp(node)
+
+        return newKey
     }
 
     override fun isEmpty() = head == null
@@ -86,33 +126,40 @@ class TreeBinaryHeap<K : Comparable<K>, V>(
         size = 0
     }
 
-    fun print() = if (isEmpty()) println("[]") else printNode(head)
-
-    private fun printNode(node: Node<K, V>?) {
-        if (node != null) {
-            println("(key = ${node.key}, value = ${node.value})\n")
-            printNode(node.left)
-            printNode(node.right)
-        }
+    override fun toString(): String {
+        if (isEmpty()) return "[]"
+        return "[ ${
+        (0 until size)
+            .map { index -> getElement(index) }
+            .joinToString { node -> "${node.key} = ${node.value}" }
+        } ]"
     }
 
     private fun getElement(index: Int): Node<K, V> {
         require(index < size) { "Index must be lesser than size" }
-        val level = log2(index.toFloat()).toInt()
-        var number = 2.times(level)
-        var i = index
+        val level = log2(index.toFloat() + 1).toInt()
+        var elemsOnLevel = 2.0.pow(level).toInt()
+        var indexOnLevel = index - (elemsOnLevel - 1)
         var node = head!!
 
-        while (number > 0) {
-            number /= 2
-            i -= number
-            node = if (i < number)
-                node.left!!
-            else
-                node.right!!
+        for (currentLevel in 0 until level) {
+            elemsOnLevel /= 2
+            if (indexOnLevel < elemsOnLevel)
+                node = node.left!!
+            else {
+                node = node.right!!
+                indexOnLevel -= elemsOnLevel
+            }
         }
-
         return node
+    }
+
+    private fun abandon(tail: Node<K, V>) {
+        if (size % 2 == 1)
+            tail.parent!!.right = null
+        else
+            tail.parent!!.left = null
+        tail.parent = null
     }
 
     private fun siftUp(node: Node<K, V>) {
@@ -148,6 +195,46 @@ class TreeBinaryHeap<K : Comparable<K>, V>(
     private fun swapContent(node1: Node<K, V>, node2: Node<K, V>) {
         node1.key = node2.key.also { node2.key = node1.key }
         node1.value = node2.value.also { node2.value = node1.value }
+    }
+
+    private fun heapify() = heapify(head!!)
+
+    private fun heapify(node: Node<K, V>?) {
+        if (node != null) {
+            siftDown(node)
+            heapify(node.left)
+            heapify(node.right)
+        }
+    }
+
+    private fun traverse(): List<Node<K, V>> {
+        val elements = mutableListOf<Node<K, V>>()
+        traverse(head, elements)
+        return elements
+    }
+
+    private fun traverse(node: Node<K, V>?, elements: MutableList<Node<K, V>>) {
+        if (node != null) {
+            elements.add(node)
+            traverse(node.left, elements)
+            traverse(node.right, elements)
+        }
+    }
+
+    private fun find(predicate: (Node<K, V>) -> (Boolean)): Node<K, V>? {
+        val result = mutableListOf<Node<K, V>>()
+        find(predicate, head, result)
+        return if (result.isEmpty()) null else result[0]
+    }
+
+    private fun find(predicate: (Node<K, V>) -> (Boolean), node: Node<K, V>?, result: MutableList<Node<K, V>>) {
+        if (node == null) return
+        if (predicate.invoke(node)) {
+            result.add(node)
+            return
+        }
+        find(predicate, node.left, result)
+        find(predicate, node.right, result)
     }
 }
 
--- src/test/kotlin/ru/spbu/datastructures/heap/TreeBinaryHeapTest.kt
+++ src/test/kotlin/ru/spbu/datastructures/heap/TreeBinaryHeapTest.kt
@@ -13,30 +13,27 @@ class TreeBinaryHeapTest {
         val emptyHeap = TreeBinaryHeap<Int, Int>()
         Assert.assertEquals(null, emptyHeap.peek())
 
-        val heap = TreeBinaryHeap<Int, Int>()
-        heap.push(1, 11)
-        heap.push(2, 22)
-        heap.push(3, 33)
-        heap.push(4, 44)
-        heap.push(5, 55)
-        heap.print()
+        val heap = TreeBinaryHeap(
+            (1..15).toList(),
+            (11..165 step 11).toList()
+        )
+        println(heap)
 
         Assert.assertEquals(11, heap.peek())
-        Assert.assertEquals(5, heap.size)
+        Assert.assertEquals(15, heap.size)
     }
 
     @Test
     fun testComparator() {
-        val heap = TreeBinaryHeap<Int, Int>(reverseOrder())
-        heap.push(5, 55)
-        heap.push(4, 44)
-        heap.push(3, 33)
-        heap.push(2, 22)
-        heap.push(1, 11)
-        heap.print()
+        val heap = TreeBinaryHeap(
+            (15 downTo 1).toList(),
+            (165 downTo 11 step 11).toList(),
+            reverseOrder()
+        )
+        println(heap)
 
-        Assert.assertEquals(55, heap.peek())
-        Assert.assertEquals(5, heap.size)
+        Assert.assertEquals(165, heap.peek())
+        Assert.assertEquals(15, heap.size)
     }
 
     @Test
@@ -51,17 +48,69 @@ class TreeBinaryHeapTest {
 
     @Test
     fun testPop() {
-        val heap = TreeBinaryHeap<Int, Int>()
-        heap.push(1, 11)
-        heap.push(2, 22)
-        heap.push(3, 33)
-        heap.push(4, 44)
-        heap.push(5, 55)
+        val heap = TreeBinaryHeap(
+            (1..15).toList(),
+            (11..165 step 11).toList()
+        )
 
         val popped = heap.pop()
 
         Assert.assertEquals(11, popped)
-        Assert.assertEquals(4, heap.size)
+        Assert.assertEquals(14, heap.size)
         Assert.assertEquals(22, heap.peek())
+    }
+
+    @Test
+    fun testReplace() {
+        val heap = TreeBinaryHeap(
+            (1..7).toList(),
+            (11..77 step 11).toList()
+        )
+        println(heap)
+
+        val replaced = heap.replace(8, 88)
+
+        println(heap)
+        Assert.assertEquals(11, replaced)
+        Assert.assertEquals(7, heap.size)
+        Assert.assertEquals(22, heap.peek())
+    }
+
+    @Test
+    fun testRemove() {
+        val heap = TreeBinaryHeap(
+            (1..7).toList(),
+            (11..77 step 11).toList()
+        )
+        println(heap)
+
+        var removed = heap.remove(55)
+
+        println(heap)
+        Assert.assertEquals(55, removed)
+        Assert.assertEquals(6, heap.size)
+        Assert.assertEquals(11, heap.peek())
+
+        removed = heap.remove(11)
+
+        println(heap)
+        Assert.assertEquals(11, removed)
+        Assert.assertEquals(5, heap.size)
+        Assert.assertEquals(22, heap.peek())
+    }
+
+    @Test
+    fun testPrioritize() {
+        val heap = TreeBinaryHeap(
+            (1..15).toList(),
+            (11..165 step 11).toList()
+        )
+        println(heap)
+
+        heap.prioritize(5, 0)
+        println(heap)
+
+        heap.prioritize(16, 0)
+        println(heap)
     }
 }
